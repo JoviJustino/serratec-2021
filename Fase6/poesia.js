84acd033df9baad89c6a875e121b4326c9974db3
JoviJustino <joaovictortere@hotmail.com>
2021-04-10 22:54:24 -0300

    fase6

---
+++ Fase6/poesia.js
@@ -0,0 +1,21 @@
+const  prompt  =  require ( "prompt-sync" ) ( ) ;
+
+
+
+var  repetir ;
+
+do {
+    console.log ( "" ) ;
+    console.log ( "E como um anjo pendeu As asas para voar" ) ;
+    
+    console.log ( "Queria a lua do céu Queria a lua do mar" ) ;
+    
+    console.log ( "" ) ;
+    console.log ( "As asas que Deus lhe deu Ruflaram de par em par" ) ;
+    
+    console.log ( "Sua alma subiu ao céu Seu corpo desceu ao mar" ) ;
+    
+    console.log ( "" ) ;
+
+    repetir  =  prompt ( "Você desejá ouvir o poema novamente?" ) ;
+}  while ( repetir  ==  "sim" ) ;
